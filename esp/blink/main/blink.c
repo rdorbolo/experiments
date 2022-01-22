b6262facbf97bc73f53c4d2510d775de1caeef7c
rick <rick@zubolu.com>
2022-01-21 21:11:02 -0800

    small test

--- esp/blink/main/blink.c
+++ esp/blink/main/blink.c
@@ -36,6 +36,6 @@ void app_main(void)
         /* Blink on (output high) */
         printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
